73c7b5bb5fc578f6dec3cdd27eb711d6beef54fe
JackPeacock <birdie.jack@gmail.com>
2014-01-19 13:09:09 +0000

    Hopefully Restructured to be exportable?

--- dartboardfunction.js
+++ dartboardfunction.js
@@ -1,8 +1,12 @@
-function findradius(xcoord, ycoord) {
+var db = db || {}; 
+
+var fs = require('fs');
+
+db.findradius = function(xcoord, ycoord) {
     return Math.sqrt((xcoord*xcoord)+(ycoord*ycoord));
 }
 
-function findtheta(xcoord, ycoord) {
+db.findtheta = function(xcoord, ycoord) {
     // Need to Correct for each quadrant else arctan(-x/-y) returns same as arctan(x/y)
     var correction = 0;
     if (xcoord < 0) {
@@ -14,11 +18,12 @@ function findtheta(xcoord, ycoord) {
     return Math.atan(ycoord/xcoord) + correction;
 }
 
-function dartboard(xcoord, ycoord) {
+db.dartboard = function(xcoord, ycoord) {
+	// Coordinate system has origin at the centre of the bullseye
     var dartboardnumbers = [6,13,4,18,1,20,5,12,9,14,11,8,16,7,19,3,17,2,15,10,6];
     // The list of dartboard numbers starting at 6 and moving anticlockwise
-	var radius = findradius(xcoord, ycoord);
-	var theta = findtheta(xcoord, ycoord);
+	var radius = db.findradius(xcoord, ycoord);
+	var theta = db.findtheta(xcoord, ycoord);
 	var segmentcounter =0;
     var dubtripfactor = 1;
     // dubtripfactor is scaling factor for double/treble beds.
@@ -54,7 +59,7 @@ function dartboard(xcoord, ycoord) {
 	while (theta > (Math.PI/20)) {
 	// theta = 0 is defined as the horizontal line running through 6.
 	// Each Number has has an angle of pi/10
-	// theta > pi/10 means the coordinates are further anticlockwise than 6.
+	// theta > pi/20 means the coordinates are further anticlockwise than 6.
 		theta = theta-(Math.PI/10);
 		segmentcounter += 1;
 	}
@@ -63,8 +68,7 @@ function dartboard(xcoord, ycoord) {
 	return number*dubtripfactor;
 }
 
-var testvalue = dartboard(16,-20);
-console.log("you scored: " + testvalue);
+module.exports = db;
 
 		
 	
