d46b4f788ad92a3c0f8d25f527258304568e7865
Jeshua Maxey <me@jeshua.co>
2014-01-19 20:03:30 +0000

    added a permanent dummy data file and cleaned up stats module

--- app.js
+++ app.js
@@ -4,34 +4,10 @@ var app = app || {};
 var fs = require('fs');
 
 /* OUR MODULES */
-var stats = require('modules/stats.js');
-var db = require('modules/dartboard.js');
+var stats = require('./modules/stats.js');
+var db = require('./modules/dartboard.js');
 
 app.N = 340; //mesh dimnesions
-
-app.mean = 0;
-app.variance = 1;
-app.stdDev = Math.sqrt(app.variance);
-
-app.gaussian1D = function(x) {
-  var m = app.stdDev * Math.sqrt(2 * Math.PI);
-  var e = Math.exp(-Math.pow(x - app.mean, 2) / (2 * app.variance));
-  return e / m;
-}
-
-app.meanX = 0;
-app.meanY = 0;
-app.varianceX = 90;
-app.varianceY = 20;
-app.stdDevX = Math.sqrt(app.varianceX);
-app.stdDevY = Math.sqrt(app.varianceY);
-
-app.gaussian2D = function(x, y, xBar, yBar) {
-	//check the normalisation constant
-  var m = app.stdDevX * app.stdDevY * Math.sqrt(2 * Math.PI);
-  var e = Math.exp( - ( Math.pow(x - xBar, 2) / (2 * app.varianceX) + Math.pow(y - yBar, 2) / (2 * app.varianceY) ) );
-  return e / m;
-}
 
 app.make2DMesh = function(size) {
 	var arr = new Array(size);
@@ -45,10 +21,17 @@ app.fillMesh = function() {
 	var mean = app.mesh.length/2;
 	for (x = 0; x < app.N; x++) {
 		for (y = 0; y < app.N; y++) {
-			//app.mesh[x][y] = 0;
 			app.mesh[x][y] = db.dartboard(x,y);
-			//app.mesh[x][y] = app.gaussian2D(x,y,mean,mean);
-			//console.log(x,y);
+			//app.mesh[x][y] = stats.gaussian2D(x,y,mean,mean);
+		};
+	};
+}
+
+app.zeroMesh = function() {
+	var mean = app.mesh.length/2;
+	for (x = 0; x < app.N; x++) {
+		for (y = 0; y < app.N; y++) {
+			app.mesh[x][y] = 0;
 		};
 	};
 }
@@ -57,9 +40,9 @@ app.addToMesh = function(meanX, meanY) {
 	//var mean = app.mesh.length/2;
 	for (var x = app.mesh.length - 1; x >= 0; x--) {
 		for (var y = app.mesh.length - 1; y >= 0; y--) {
-			app.mesh[x][y] += app.gaussian2D(x,y,meanX,meanY)*db.dartboard(x,y);
-		};
-	};
+			app.mesh[x][y] += stats.gaussian2D(x,y,meanX,meanY,400,400);//*db.dartboard(x,y);
+		}
+	}
 }
 
 app.writeToFile = function() {
@@ -79,18 +62,24 @@ app.randomInt = function(randMax) {
 	return Math.floor(Math.random()*randMax);
 }
 
+app.fakeData = function(n) {
+	for(c=0;c<n;c++) {
+		var x = app.randomInt(app.N);
+		var y = app.randomInt(app.N);
+		app.addToMesh(x,y);
+	}
+}
+
 //SCRIPT BIT
 
 app.mesh = app.make2DMesh(app.N);
+app.zeroMesh();
 app.fillMesh();
-app.writeToFile();
 
-//	//uncomment to fill with fake data
-//for(c=0;c<1000;c++) {
-//	var x = app.randomInt(app.N);
-//	var y = app.randomInt(app.N);
-//	app.addToMesh(x,y);
-//}
+//uncomment to fill darts.json with fake data
+//app.fakeData(400);
+
+app.writeToFile();
 
 //END SCRIPT BIT
 module.exports = app;
--- modules/stats.js
+++ modules/stats.js
@@ -1,3 +1,26 @@
 var stats = stats || {};
 
+stats.gaussian1D = function(x, meanX, varX) {
+	var meanX = meanX || 0;
+	var varX = varX || 1;
+	var stdDev = Math.sqrt(varX);
+
+  var m = stats.stdDev * Math.sqrt(2 * Math.PI);
+  var e = Math.exp(-Math.pow(x - stats.mean, 2) / (2 * stats.variance));
+  return e / m;
+}
+
+stats.gaussian2D = function(x, y, meanX, meanY, varX, varY) {
+	var meanX = meanX || 0;
+	var meanY = meanY || 0;
+	var varX = varX || 1;
+	var varY = varY || 1;
+	var sdX = Math.sqrt(varX);
+	var sdY = Math.sqrt(varY);
+	//check the normalisation constant
+  var e = Math.exp( - ( Math.pow(x - meanX, 2) / (2 * sdX) + Math.pow(y - meanY, 2) / (2 * sdY) ) );
+  var m = sdX * sdY * Math.sqrt(2 * Math.PI);
+  return e / m;
+}
+
 module.exports = stats;
