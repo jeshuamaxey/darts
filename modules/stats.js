3db7714d9778780a495a8a1ec44f3a5f95f2531e
Jeshua Maxey <me@jeshua.co>
2014-01-21 16:48:11 +0000

    commented all functions. Added a function to calculate the error function numerically. Added a private object which isn't exported with the stats module to module users

--- modules/stats.js
+++ modules/stats.js
@@ -1,15 +1,38 @@
-var stats = stats || {};
+/*
+* All private variables and functions are attached to the priv object
+* which is not exported
+*/
 
-stats.gaussian1D = function(x, meanX, varX) {
+var priv = priv || {};
+
+//stores previously calculated values of the factorial function
+priv.f = [];
+
+/*
+* All public variables and functions are attached to the stats object
+* which is exported at the end of the module
+*/
+var stats = stats || {};
+/*
+* Returns the value of a 1D Gaussian PDF, centerd at meanX
+* and standard deviations sdX, at x. When only x is
+* provided as an argument it defaults to a normal distribution 
+*/
+stats.gaussian1D = function(x, meanX, sdX) {
 	var meanX = meanX || 0;
-	var varX = varX || 1;
-	var stdDev = Math.sqrt(varX);
+	var sdX = sdX || 1;
+	var varX = sdX*sdX;
 
   var m = stats.stdDev * Math.sqrt(2 * Math.PI);
   var e = Math.exp(-Math.pow(x - stats.mean, 2) / (2 * stats.variance));
   return e / m;
 }
 
+/*
+* Returns the value of a 2D Gaussian PDF, centerd at (meanX,meanY)
+* and standard deviations sdX & sdY, at (x,y). When only x & y are
+* provided as arguments it defaults to a normal distribution 
+*/
 stats.gaussian2D = function(x, y, meanX, meanY, sdX, sdY) {
 	var meanX = meanX || 0;
 	var meanY = meanY || 0;
@@ -23,8 +46,35 @@ stats.gaussian2D = function(x, y, meanX, meanY, sdX, sdY) {
   return ( (e / m) < 0.00001 ? 0 : e/m );
 }
 
+/*
+*
+*
+*/
 stats.calcStandardDev = function(accuracy) {
 	return config.meshSize*config.meshRatio.bullseye/0.4;
+}
+
+/*
+* Returns th value of the error function, accurate to 9 decimal places
+*/
+stats.erf = function(x) {
+	var k=0, el=0, sum=0;
+	do {
+		el = ( Math.pow(-1,k) / ( (2*k +1)*stats.factorial(k) ) ) * Math.pow(x, 2*k);
+		sum += el;
+		k++;
+	} while(el > 0.000000001)
+	return ((2*x/Math.sqrt(Math.PI))*sum).toFixed(9);
+}
+
+/*
+*/
+stats.factorial = function(n) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
 }
 
 module.exports = stats;
