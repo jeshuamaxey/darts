20c908d92b43c3ef392364065b41a8a515d7c467
Jeshua Maxey <me@jeshua.co>
2014-01-21 16:56:39 +0000

    added a script (in comments) to programatically produce all data for accuracy ranging from 0.0 to 1.00 at 0.01 intervals. Not to be run until model has been fully tested

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var config = require('./modules/config.js');
 var stats = require('./modules/stats.js');
 var db = require('./modules/dartboard.js');
 
+// Read in the mesh size
 app.N = config.meshSize;
 
 app.make2DMesh = function(size) {
@@ -29,7 +30,7 @@ app.fillMesh = function() {
 
 app.generateHeatmap = function(sdX, sdY) {
 	for (var x = 0; x < app.N; x++) {
-		console.log("calculating weight for "+x+")");
+		console.log("calculating weight for "+x+"th row");
 		for (var y = 0; y < app.N; y++) {
 			app.weight(x,y, sdX, sdY);
 		}
@@ -88,6 +89,7 @@ app.fakeData = function(n) {
 //SCRIPT BIT
 
 app.mesh = app.make2DMesh(app.N);
+
 app.zeroMesh();
 
 var acc = 0.31;
@@ -96,8 +98,24 @@ var sdY = config.meshSize*config.meshRatio.bullseye/0.4;
 
 app.generateHeatmap(sdX, sdY);
 
-var fileName = 'acc='+acc+'.json';
+var fileName = 'acc-'+acc+'.json';
 app.writeToFile(fileName);
+
+var acc, sdX, sdY, fileName;
+
+// for(var c=0; c<1000; c++) {
+// 	//reset mesh
+// 	app.zeroMesh();
+// 	//update model variables
+// 	acc = (c*0.01).toFixed(2);
+// 	sdX = stats.erf(acc);
+// 	sdY = stats.erf(acc);
+// 	//crunch da numberz
+// 	app.generateHeatmap(sdX, sdY);
+// 	//output data
+// 	fileName = 'acc-'+acc+'.json';
+// 	app.writeToFile(fileName);
+// }
 
 //END SCRIPT BIT
 module.exports = app;
