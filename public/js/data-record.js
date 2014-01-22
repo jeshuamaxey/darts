a127a4fb5807227f803738e8d5cdf3eddfcc25d0
Jeshua Maxey <me@jeshua.co>
2014-01-22 14:25:01 +0000

    added a clibration routine which records the postition of 5 points on the board. Doesn't do anything with this information just yet

--- public/js/data-record.js
+++ public/js/data-record.js
@@ -21,6 +21,9 @@ app.videoContraints = {
 }
 
 app.main = function() {
+	app.video = $('#vid')[0];
+	app.overlay = $('#overlay');
+
 	$('#startStream').on('click', app.startVideo);
 	$('#stopStream').on('click', app.stopVideo);
 	$('#startCalib').on('click', app.startCalib);
@@ -30,13 +33,13 @@ app.main = function() {
 * VIDEO FUNCTIONS
 */
 app.startVideo = function() {
-	app.video = document.getElementById('vid');
-
-	navigator.getUserMedia(app.videoContraints, function(stream) {
-		app.videoShowing = true;
-		app.stream = stream;
-	  app.video.src = window.URL.createObjectURL(app.stream);
-	}, app.errorCallback);
+	if(!app.videoShowing) {
+		navigator.getUserMedia(app.videoContraints, function(stream) {
+			app.videoShowing = true;
+			app.stream = stream;
+		  app.video.src = window.URL.createObjectURL(app.stream);
+		}, app.errorCallback);
+	}
 }
 
 app.stopVideo = function() {
@@ -57,13 +60,29 @@ app.startCalib = function() {
 		alert('You need to start the video before you can calibrate');
 		return 0;
 	}
-	app.calibrationPoint = 0;
-	$('#calibInstructions').show();
-	$('#overlay').on('click', app.calibClick);
+	app.calibClicks = [];
+	$('#calibDialog').show();
+	app.overlay.on('click', app.recordClick);
 }
 
-app.calibClick = function() {
+app.calculateCalibration = function() {
+	console.log(app.calibClicks)
+	// //convert to cartisian coords centred on the bull
+	// click = {'x' : e.offsetX, 'y' : e.offsetY}
+}
 
+app.recordClick = function(e) {
+	console.log('testing')
+	//record calibration point data
+	app.calibClicks.push(e);
+	//update calibration dialog
+	$('#calibPoints li.clickHere').removeClass('clickHere').addClass('clicked');
+	$($('#calibPoints li')[app.calibClicks.length]).addClass('clickHere');
+	//if we have all the calibration points, unbind the event handler and calculate
+	if(app.calibClicks.length==5) {
+		app.calculateCalibration();
+		app.overlay.unbind('click');
+	}
 }
 
 //must go last
