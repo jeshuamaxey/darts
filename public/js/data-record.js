94bb45f73c2562a4c1ca407f54c6799123871ab8
Jeshua Maxey <me@jeshua.co>
2014-01-22 17:06:39 +0000

    changed tack. Plan is to position overlay with ccs tranforms over the dartboard and calibrate that way. Headway made in this direction. Rotation is possible, now to add translation

--- public/js/data-record.js
+++ public/js/data-record.js
@@ -20,9 +20,15 @@ app.videoContraints = {
   }
 }
 
+app.overlayRy = 0;
+app.overlayRx = 0;
+
 app.main = function() {
 	app.video = $('#vid')[0];
 	app.overlay = $('#overlay');
+
+	app.ovCanvas = app.overlay[0];
+	app.ovCtx = app.ovCanvas.getContext('2d');
 
 	$('#startStream').on('click', app.startVideo);
 	$('#stopStream').on('click', app.stopVideo);
@@ -60,8 +66,12 @@ app.startCalib = function() {
 		alert('You need to start the video before you can calibrate');
 		return 0;
 	}
+	app.redrawGrid(app.ovCanvas, app.ovCtx);
 	app.calibClicks = [];
 	$('#calibDialog').show();
+	$('#yRotation').on('change', app.updateRotation);
+	$('#xRotation').on('change', app.updateRotation);
+	$('#resetRotation').on('click', app.resetRotation)
 	app.overlay.on('click', app.recordClick);
 }
 
@@ -72,7 +82,7 @@ app.calculateCalibration = function() {
 }
 
 app.recordClick = function(e) {
-	console.log('testing')
+	console.log(e.offsetX, e.offsetY)
 	//record calibration point data
 	app.calibClicks.push(e);
 	//update calibration dialog
@@ -83,6 +93,58 @@ app.recordClick = function(e) {
 		app.calculateCalibration();
 		app.overlay.unbind('click');
 	}
+}
+
+app.updateRotation = function() {
+	app.overlayRx = $('#xRotation').val()
+	app.overlayRy = $('#yRotation').val()
+	$('span#xRotationDisp').html(app.overlayRx);
+	$('span#yRotationDisp').html(app.overlayRy);
+	$('#overlay').css('-webkit-transform','rotateX('+app.overlayRx+'deg) rotateY('+app.overlayRy+'deg)')
+}
+
+app.resetRotation = function() {
+	$('span#yRotationDisp').html(0);
+	$('span#xRotationDisp').html(0);
+	$('#overlay').css('-webkit-transform','rotateX('+0+'deg) rotateY('+0+'deg)')
+}
+
+app.redrawGrid = function(canvas, ctx) {
+	var w = canvas.width;
+	var h = canvas.height;
+
+	var x = w/2;
+	var y = h/2;
+
+	for (var rad = 0; rad < w/2; rad += 8) {
+		app.drawCircle(ctx, x, y, rad)
+	}
+
+	//add some text to keep track of fowards face
+	ctx.fillText("THIS FACE FORWARDS", 10, 10)
+
+	//	cartesian grid lines
+	// for (var x = 0; x < w; x += 10) {
+	//   ctx.moveTo(x, 0);
+	//   ctx.lineTo(x, h);
+	// }
+
+	// for (var y = 0.5; y < h; y += 10) {
+	//   ctx.moveTo(0, y);
+	//   ctx.lineTo(w, y);
+	// }
+	//draw it
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = "#ddd";
+	ctx.stroke();
+}
+
+app.drawCircle = function(ctx, x, y, rad) {
+	ctx.beginPath();
+	ctx.lineWidth = 1;
+	ctx.arc(x, y, rad, 0, Math.PI*2, true);
+	ctx.stroke();
+	ctx.closePath();
 }
 
 //must go last
