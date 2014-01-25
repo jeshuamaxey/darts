ea4a1557edacebe62165813cdb00093bca719c70
Jeshua Maxey <me@jeshua.co>
2014-01-25 13:22:34 +0000

    changed server to output port number

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000);
+app.listen(port);
+
+console.log("Server running at http://localhost:" + port)
