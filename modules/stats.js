bdada4895e7539ff45fbd5a9d2d7adc157284e19
Jeshua Maxey <me@jeshua.co>
2014-01-27 21:43:44 +0000

    fixed factorial formula

--- modules/stats.js
+++ modules/stats.js
@@ -70,11 +70,11 @@ stats.erf = function(x) {
 /*
 */
 stats.factorial = function(n) {
-  if (n == 0 || n == 1)
+  if (n == 1 || n == 0)
     return 1;
-  if (f[n] > 0)
-    return f[n];
-  return f[n] = factorial(n-1) * n;
+  if (priv.f[n] > 0)
+    return priv.f[n];
+  return priv.f[n] = stats.factorial(n-1) * n;
 }
 
 module.exports = stats;
