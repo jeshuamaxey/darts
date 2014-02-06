c39c5a60f30d4ed4d60f089aa77c67e0d84fe4be
JackPeacock <birdie.jack@gmail.com>
2014-02-06 14:43:23 +0000

    Fixed quite a few bugs.

--- test.js
+++ test.js
@@ -4,12 +4,13 @@
 */
 
 var db = require('./modules/dartboard.js')
+var config = require('./modules/config.js')
 var a = 0;
 
 console.time('first');
 
-for(x=0;x<340;x++) {
-	for(y=0;y<340;y++) {
+for(x=0;x<config.meshSize;x++) {
+	for(y=0;y<config.meshSize;y++) {
 		a = db.dartboard(x,y);
 	}
 }
@@ -17,8 +18,8 @@ for(x=0;x<340;x++) {
 console.timeEnd('first');
 console.time('Second')
 
-for(x=0;x<340;x++) {
-	for(y=0;y<340;y++) {
+for(x=0;x<config.meshSize;x++) {
+	for(y=0;y<config.meshSize;y++) {
 		a = db.dartboard(x,y);
 	}
 }
@@ -26,8 +27,8 @@ for(x=0;x<340;x++) {
 console.timeEnd('Second')
 console.time('Third')
 
-for(x=0;x<340;x++) {
-	for(y=0;y<340;y++) {
+for(x=0;x<config.meshSize;x++) {
+	for(y=0;y<config.meshSize;y++) {
 		a = db.dartboard(x,y);
 	}
 }
