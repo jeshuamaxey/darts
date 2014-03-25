e80266f0190749105d8e2c9bf10fbb72acec42c4
JackPeacock <birdie.jack@gmail.com>
2014-03-25 18:04:17 +0000

    Commit to sync

--- Jack's files for integrating/testingGM.js
+++ Jack's files for integrating/testingGM.js
@@ -1,9 +1,4 @@
 var hm = require('../modules/heatmap.js');
 var fs = require('fs');
 
-var sd = 1;
-
-for (i =0; i < 3; i++) {
-	hm.generateHeatmap({x: 0, y: 0}, {x: 16+i/2, y: 16+i/2});
-	console.log(i);
-}
+hm.generateHeatmap({x: 0, y: 0}, {x: 10, y: 10});
