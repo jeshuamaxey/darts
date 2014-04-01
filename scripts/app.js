8225530325dccebf544be503fb2756b906bd977b
Jeshua Maxey <me@jeshua.co>
2014-04-01 11:27:51 +0100

    removed old arraymax function file (moved into mesh module). tuned app.js to spit out loads of heatmaps

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,7 @@ var fs = require('fs');
 var config = require('../modules/config.js');
 var hm = require('../modules/heatmap.js');
 var mesh = require('../modules/mesh.js');
+var files = require('../modules/files.js');
 
 /*
 * This gets called
@@ -19,24 +20,29 @@ app.main = function() {
 	var sd;
 	//if you want to do use sd = z then set it so:
 	//var sdMin = x, sdMax = x+sdStep, sdStep = whatever;
-	var sdXMin = 50.5, sdXMax = 51.0,
-			sdYMin = 50.5, sdYMax = 51.0,
+	var sdXMin = 10.0, sdXMax = 100.5,
+			sdYMin = 10.0, sdYMax = 100.5,
 			sdStep = 0.5;
 
 	var loopLimY = (sdYMax-sdYMin)/sdStep;
 
 	for(var d=0; d<loopLimY; d++) {
-		console.log("d = "+d);
 		//set standard deviation
 		sd = app.setStdDev(sdXMin + d*sdStep, sdYMin + d*sdStep);
 		mean = app.setMean(0, 0);
-		//crunch da numberz
-		hm.generateHeatmap(mean, sd);
-		//write data to file
+		//set file names
 		var fileName = files.generateFileName(sd);
-		var dirName = (config.meshSize == 400) ? '../public/data/symmetric/res400' : '../public/data/symmetric'
-		files.writeToFile(mesh, fileName, dirName);
-
+		var dirName = (config.meshSize == 400) ? config.dataDir + 'symmetric/res400' : config.dataDir + '/symmetric';
+		//only bother to generate data if the file doesn't already exist
+		if(!fs.existsSync(dirName+'/'+fileName)) {
+			console.log(dirName+'/'+fileName + " doesn't exist");
+			//crunch da numberz
+			hm.generateHeatmap(mean, sd);
+			//write data to file
+			files.writeToFile(mesh, fileName, dirName);
+		} else {
+			console.log(dirName+'/'+fileName + " does exist. Skipping over it")
+		}
 	}
 }
 
