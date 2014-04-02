e36cb503bf8f2f4960daa9a466b2f3a8400aca1d
Jeshua Maxey <me@jeshua.co>
2014-04-02 11:45:53 +0100

    improved differentiation plot

--- public/js/build/plots.js
+++ public/js/build/plots.js
@@ -96,6 +96,8 @@ if (document.location.hostname == "localhost") {
 
 //run on page load
 plots.main = function() {
+	$('#stdDev').on('change', plots.updatePlot);
+
 	plots.db = {
 		"width" : $('#dartboard').width(),
 		"height" : $('#dartboard').height()
@@ -118,21 +120,34 @@ plots.main = function() {
 	})
 	//
 	.done(function(data) {
+		plots.data = data;
+
 		var x = data[0].coords.x*plots.pixelSize;
 		var y = data[0].coords.y*plots.pixelSize;
-
-		plots.dbCtx.beginPath()
-		plots.dbCtx.moveTo(x, y);
+		var old = {'x':0, 'y':0};
 		
+		//draw points
 		data.forEach(function(el) {
 			x = el.coords.x*plots.pixelSize;
 			y = el.coords.y*plots.pixelSize;
-			/*
-			* these currently don't plot both at the same time
-			* comment out line 51 to see line
-			*/
-			//draw.circle(plots.dbCtx, x, y, .5);
-			plots.dbCtx.lineTo(x, y);
+			draw.circle(plots.dbCtx, x, y, .5);
+		});
+
+		//draw line
+		plots.dbCtx.beginPath()
+		plots.dbCtx.moveTo(x, y);
+
+		data.forEach(function(el, i) {
+			old.x = x;
+			old.y = y;
+			x = el.coords.x*plots.pixelSize;
+			y = el.coords.y*plots.pixelSize;
+			//calc difference between points
+			dx = Math.abs(old.x - x);
+			dy = Math.abs(old.y - y);
+			diff = Math.pow(dx*dx + dy*dy, 0.5);
+			if(diff>5*plots.pixelSize) plots.dbCtx.moveTo(x, y);
+			else plots.dbCtx.lineTo(x, y);
 		});
 		
 		plots.dbCtx.strokeStyle = '#000';
@@ -141,6 +156,17 @@ plots.main = function() {
 	});
 }
 
+//
+plots.updatePlot = function() {
+	var sd = parseFloat($(this).val());
+	$('.stdDevDisp').html(sd.toFixed(1));
+	var i = sd/0.5 - 1;
+	var x = plots.data[i].coords.x*plots.pixelSize;
+	var y = plots.data[i].coords.y*plots.pixelSize;
+	plots.dbCtx.strokeStyle = '#f00';
+	draw.circle(plots.dbCtx, x, y, 2);
+	plots.dbCtx.strokeStyle = '#fff'
+}
 
 //
 $(document).ready(plots.main);
