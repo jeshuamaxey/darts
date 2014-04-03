91be632dbe19f5379f03fcd919478690496441a9
Jeshua Maxey <me@jeshua.co>
2014-04-04 00:45:05 +0100

    updated app.js to allow for looping through different covariance values

--- scripts/app.js
+++ scripts/app.js
@@ -17,20 +17,30 @@ var files = require('../modules/files.js');
 * This gets called
 */
 app.main = function() {
-	var sd, mesh;
+	var sd, cov, mesh;
 	//if you want to do use sd = z then set it so:
-	//var sdMin = x, sdMax = x+sdStep, sdStep = whatever;
+	//var sdMin = z, sdMax = z+sdStep, sdStep = whatever;
 	var sdXMin = 35, sdXMax = 35.5,
 			sdYMin = 34, sdYMax = 34.5,
 			sdStep = 0.5;
 
-	var cov = -0.55;
+	//if you want to do use cov = z then set it so:
+	//var covMin = z, covMax = z+covStep, covStep = whatever;
+	var covMin = -1, covMax = 1,
+			covStep = 0.1;
 
-	var loopLimY = (sdYMax-sdYMin)/sdStep;
+	//(un)comment based on whether you're looping through cov or sd
+	//var loopLim = (sdYMax-sdYMin)/sdStep;
+	var loopLim = (covMax-covMin)/covStep;
+	sd = app.setStdDev(100, 100);
+	//cov = 0;
 
-	for(var d=0; d<loopLimY; d++) {
+	for(var d=0; d<loopLim; d++) {
 		//set standard deviation
-		sd = app.setStdDev(sdXMin + d*sdStep, sdYMin + d*sdStep);
+		//(un)comment based on whether you're looping through cov or sd
+		//sd = app.setStdDev(sdXMin + d*sdStep, sdYMin + d*sdStep);
+		cov = covMin + d*covStep
+
 		mean = app.setMean(9, -5);
 		//set file names
 		var fileName = files.generateFileName(sd, cov);
