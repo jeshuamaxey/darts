404de7a67ca4bf3e364c3eda113f8b01e5633ae2
Jeshua Maxey <me@jeshua.co>
2014-04-07 19:40:40 +0100

    preliminary correlation plots produced. Need fine tuning

--- public/js/build/plots.js
+++ public/js/build/plots.js
@@ -108,13 +108,19 @@ plots.main = function() {
 
 	//initialise canvases
 	plots.sdDbCanvas = document.getElementById('sdDartboard');
-	plots.dbCtx = plots.sdDbCanvas.getContext('2d');
-	plots.ovCanvas = document.getElementById('overlay');
-	plots.ovCtx = plots.ovCanvas.getContext('2d');
+	plots.sdDbCtx = plots.sdDbCanvas.getContext('2d');
+	plots.sdOverlayCanvas = document.getElementById('sdOverlay');
+	plots.sdOverlayCtx = plots.sdOverlayCanvas.getContext('2d');
+
+	plots.corDbCanvas = document.getElementById('corDartboard');
+	plots.corDbCtx = plots.corDbCanvas.getContext('2d');
+	plots.corOverlayCanvas = document.getElementById('corOverlay');
+	plots.corOverlayCtx = plots.corOverlayCanvas.getContext('2d');
 
 	//draw plots
-	plots.plotDartboard();
+	plots.plotAimVsSdDb();
 	plots.plotLineGraph();
+	plots.plotAimVsCorDb();
 
 	//setup event listeners
 	$('.panel-collapse').on('hide.bs.collapse', function () {
@@ -127,10 +133,12 @@ plots.main = function() {
 	});
 }
 
-//
-plots.plotDartboard = function() {
+/*
+* generates the plot of optimum aim as a function of sd
+*/
+plots.plotAimVsSdDb = function() {
 	//draw a dartboard onto the canvas
-	draw.dartBoard(plots.dbCtx, plots.sdDbCanvas, plots.db, true, true);
+	draw.dartBoard(plots.sdDbCtx, plots.sdDbCanvas, plots.db, true, true);
 
 	//fetch the max expected data
 	url = plots.dataLocation + 'max-expected.json';
@@ -140,62 +148,66 @@ plots.plotDartboard = function() {
 	})
 	//
 	.done(function(data) {
-		plots.data = data;
+		plots.sdData = data;
 
-		var x = data[0].coords.x*plots.pixelSize;
-		var y = data[0].coords.y*plots.pixelSize;
+		var x = plots.sdData[0].coords.x*plots.pixelSize;
+		var y = plots.sdData[0].coords.y*plots.pixelSize;
 		var old = {'x':0, 'y':0};
 		
 		//draw points
-		data.forEach(function(el) {
-			x = el.coords.x*plots.pixelSize;
-			y = el.coords.y*plots.pixelSize;
-			draw.circle(plots.dbCtx, x, y, .5);
+		plots.sdData.forEach(function(point) {
+			x = point.coords.x*plots.pixelSize;
+			y = point.coords.y*plots.pixelSize;
+			draw.circle(plots.sdDbCtx, x, y, .5);
 		});
 
 		//draw line
-		plots.dbCtx.beginPath()
-		plots.dbCtx.moveTo(x, y);
+		plots.sdDbCtx.beginPath()
+		plots.sdDbCtx.moveTo(x, y);
 
-		data.forEach(function(el, i) {
+		plots.sdData.forEach(function(point, i) {
 			old.x = x;
 			old.y = y;
-			x = el.coords.x*plots.pixelSize;
-			y = el.coords.y*plots.pixelSize;
+			x = point.coords.x*plots.pixelSize;
+			y = point.coords.y*plots.pixelSize;
 			//calc difference between points
 			dx = Math.abs(old.x - x);
 			dy = Math.abs(old.y - y);
 			diff = Math.pow(dx*dx + dy*dy, 0.5);
-			if(diff>5*plots.pixelSize) plots.dbCtx.moveTo(x, y);
-			else plots.dbCtx.lineTo(x, y);
+			if(diff>5*plots.pixelSize) plots.sdDbCtx.moveTo(x, y);
+			else plots.sdDbCtx.lineTo(x, y);
 		});
 		
-		plots.dbCtx.strokeStyle = '#000';
-		plots.dbCtx.closePath();
-		plots.dbCtx.stroke();
+		plots.sdDbCtx.strokeStyle = '#000';
+		plots.sdDbCtx.closePath();
+		plots.sdDbCtx.stroke();
 	});
 }
 
-//
+/*
+*
+*/
 plots.updateOverlay = function() {
 	//clear canvas
-	draw.clear(plots.ovCtx, plots.ovCanvas);
+	draw.clear(plots.sdOverlayCtx, plots.sdOverlayCanvas);
 	//get all the info we need
 	var sd = parseFloat($(this).val());
 	var i = sd/0.5 - 1;
-	var x = plots.data[i].coords.x*plots.pixelSize;
-	var y = plots.data[i].coords.y*plots.pixelSize;
-	var maxExp = plots.data[i].max;
+	var x = plots.sdData[i].coords.x*plots.pixelSize;
+	var y = plots.sdData[i].coords.y*plots.pixelSize;
+	var maxExp = plots.sdData[i].max;
 	//update displays
 	$('#stdDbDevDisp').html(sd.toFixed(1));
 	$('.maxExpDisp').html(maxExp.toFixed(2));
 	//update canvas
-	plots.ovCtx.strokeStyle = '#f00';
-	draw.circle(plots.ovCtx, x, y, 2);
-	plots.ovCtx.strokeStyle = '#fff'
+	plots.sdOverlayCtx.strokeStyle = '#f00';
+	draw.circle(plots.sdOverlayCtx, x, y, 2);
+	plots.sdOverlayCtx.strokeStyle = '#fff'
 }
 
-//
+/*
+* generates the plot of max expected vs sd
+*/
 plots.plotLineGraph = function() {
 	var margin = {top: 80, right: 20, bottom: 30, left: 50},
     width = 800 - margin.left - margin.right,
@@ -254,6 +266,92 @@ plots.plotLineGraph = function() {
 	      .attr("class", "line")
 	      .attr("d", line);
 	});
+}
+
+/*
+* generates the plot of optimum aim as a function of
+* correlation for different standard deviations
+*/
+plots.plotAimVsCorDb = function() {
+	//draw a dartboard onto the canvas
+	draw.dartBoard(plots.corDbCtx, plots.corDbCanvas, plots.db, true, true);
+
+	//fetch the max expected data
+	url = plots.dataLocation + 'max-vs-cor.json';
+	$.ajax({
+		url: url,
+		cache: false
+	})
+	//
+	.done(function(data) {
+		plots.corData = data;
+
+//		plotCovSeries(plots.corData[1])
+
+		plots.corData.forEach(plotCovSeries)
+	});
+}
+
+/*
+* plot a single series of covariance data
+*/
+function plotCovSeries(series, i, arr) {
+
+	var index = (i+1)/(arr.length+1);
+	console.log(i+1 + '/'+ (arr.length+1) + '=' + index)
+	var colour = plots.fullColor(index)
+
+	//console.log(series)
+	var x = series.data[0].coords.x*plots.pixelSize;
+	var y = series.data[0].coords.y*plots.pixelSize;
+	var old = {'x':0, 'y':0};
+
+	series.data.forEach(function(point) {
+		//draw points
+		x = point.coords.x*plots.pixelSize;
+		y = point.coords.y*plots.pixelSize;
+		draw.circle(plots.corDbCtx, x, y, .5);
+	});
+
+	//draw line
+	plots.corDbCtx.beginPath()
+	plots.corDbCtx.moveTo(x, y);
+
+	series.data.forEach(function(point, i) {
+		old.x = x;
+		old.y = y;
+		x = point.coords.x*plots.pixelSize;
+		y = point.coords.y*plots.pixelSize;
+		//calc difference between points
+		// dx = Math.abs(old.x - x);
+		// dy = Math.abs(old.y - y);
+		// diff = Math.pow(dx*dx + dy*dy, 0.5);
+		// if(diff>5*plots.pixelSize) plots.corDbCtx.moveTo(x, y);
+		// else plots.corDbCtx.lineTo(x, y);
+		plots.corDbCtx.lineTo(x, y);
+	});
+
+	plots.corDbCtx.strokeStyle = colour;
+	plots.corDbCtx.closePath();
+	plots.corDbCtx.stroke();
+	
+}
+
+//takes a value in the range 0-1
+plots.fullColor = function(val) {
+	var c = Math.floor(5*255*val);
+	var r = 255, g = 255, b = 255;
+	while(c > 0) {
+		if(r!=0 	&& g==255 && b==255)		r--;	//rgb(255,255,255)		-> rgb(0,255,255)
+		if(r==0 	&& g==255 && b!=0) 		b--;		//rgb(0,255,0)				-> rgb(0,255,0)
+		if(r!=255 && g==255 && b==0) 		r++;		//rgb(0,255,0)				-> rgb(255,255,0)
+		if(r==255 && g!=0 	&& b==0) 		g--;		//rgb(255,255,0)			-> rgb(255,0,	0)
+		if(r==255 && g==0 	&& b!=255)	b++;		//rgb(255,0,0)				-> rgb(255,0,255)
+		c--;
+	}
+	var color = 'rgba('+r+','+g+','+b+',1)';
+	//var color = 'rgba('+88+','+shade+','+shade+',1)';
+	return color;
 }
 
 //
